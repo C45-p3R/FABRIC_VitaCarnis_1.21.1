ff47a60359f5a52b49ce22aa710524da9fcce8ee
KhDanial <242464@astanait.edu.kz>
2025-05-30 21:00:11 +0500

    Initial commit

---
+++ src/main/java/net/casper/vitacarnis/VitaCarnis.java
@@ -0,0 +1,16 @@
+package net.casper.vitacarnis;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VitaCarnis implements ModInitializer {
+	public static final String MOD_ID = "vita-carnis";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/casper/vitacarnis/VitaCarnisClient.java
@@ -0,0 +1,10 @@
+package net.casper.vitacarnis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VitaCarnisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
