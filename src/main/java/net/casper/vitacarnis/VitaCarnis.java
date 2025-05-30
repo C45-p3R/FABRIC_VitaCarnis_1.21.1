f82d5f2cb3e26a7b657a58bc85dc1f1ba684e862
KhDanial <182223761+C45-p3R@users.noreply.github.com>
2025-05-30 21:12:45 +0500

    commit test

--- src/main/java/net/casper/vitacarnis/VitaCarnis.java
+++ src/main/java/net/casper/vitacarnis/VitaCarnis.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class VitaCarnis implements ModInitializer {
 	public static final String MOD_ID = "vita-carnis";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	//test
 
 	@Override
 	public void onInitialize() {
